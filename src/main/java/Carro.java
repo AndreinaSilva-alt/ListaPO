24da1a3c9b0cf22d8b185537f12b5e2f1ebab470
Andreina Kelly De Nascimento Da Silva - FATEC - Cuiaba/MT <00615599@alunosenai.mt>
2026-03-23 21:24:39 -0400

    exercio2

--- src/main/java/Carro.java
+++ src/main/java/Carro.java
@@ -2,4 +2,14 @@ public class Carro {
     String marca;
     String modelo;
     int ano;
+
+    public int idadeDoCarro(){
+        return 2026 - ano;
+    }
+    public void exibir() {
+        System.out.println("marca: " + this.marca);
+        System.out.println("modelo " + this.modelo);
+        System.out.println("ano " +  this.ano);
+        System.out.println("A idade do carro é " + idadeDoCarro());
+    }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,14 +4,14 @@ public class Main {
         carro1.marca = "toyota";
         carro1.modelo = "corolla";
         carro1.ano = 2020;
-        System.out.println("marca: " + carro1.marca + " modelo: " + carro1.modelo  +" ano: " + carro1.ano );
-
+        carro1.exibir();
 
         Carro carro2 = new Carro();
         carro2.marca = "BMW";
         carro2.modelo = "Range Rover";
         carro2.ano = 2015;
-        System.out.println("marca: " + carro2.marca + " modelo: " + carro2.modelo + " ano " + carro2.ano );
+        carro2.exibir();
+
     }
 }
 
